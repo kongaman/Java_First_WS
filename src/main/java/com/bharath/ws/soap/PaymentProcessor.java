1550d5d1fc2e4208e85a33b92aa8098a022ab347
kongaman <christian.kranz@gmail.com>
2019-01-11 15:24:51 +0100

    Marked the interface with JAX-WS annotations

--- src/main/java/com/bharath/ws/soap/PaymentProcessor.java
+++ src/main/java/com/bharath/ws/soap/PaymentProcessor.java
@@ -1,9 +1,20 @@
 package com.bharath.ws.soap;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
 import com.bharath.ws.soap.dto.PaymentProcessorRequest;
 import com.bharath.ws.soap.dto.PaymentProcessorResponse;
 
+
+//Implementation doesnt have to be annotated, any class that implements the interface will be exposed as a webservice
+@WebService(name = "PaymentProcessor")
 public interface PaymentProcessor {
 
-	public PaymentProcessorResponse processPayment(PaymentProcessorRequest paymentProcessorRequest);
+	@WebMethod
+	public @WebResult(name = "result") PaymentProcessorResponse processPayment(
+			@WebParam(name="paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest);
+	//@WebMethod, @WebResult and @WebParam are optional (only to control the name in the wsdl)
 }
